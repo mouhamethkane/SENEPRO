2243ed7f34ca2c928c1dbd0f08c494cb696fcea3
Dammel-Meuz <ndoyemamemoussa@gmail.com>
2023-08-01 02:56:03 +0000

    conflit update

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,14 @@ Route::get('/', function () {
     return view('ouvriers.accueil');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
