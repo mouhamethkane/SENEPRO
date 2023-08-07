d0fef371a6709a0539ac8bb55d799bf9ea5ec988
unknown <gningmasseye10@gmail.com>
2023-08-07 14:13:07 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
-<<<<<<< HEAD
-use App\Http\Controllers\ProfileController;
-=======
 use App\Http\Controllers\MailController;
->>>>>>> sendmail
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|-----------------------------------s---------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
@@ -21,24 +18,13 @@ Route::get('/', function () {
     return view('ouvriers.accueil');
 });
 
-<<<<<<< HEAD
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
-=======
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
-Route::get('send-mail',[MailController::class, 'index']);
-Route::get('/contact',[MailController::class, 'contact'])->name('contactpage');
-Route::post('/contact',[MailController::class, 'contactSend'])->name('contactsendpage');
->>>>>>> sendmail
+
+
+Route::get('/contact', [MailController::class, 'contact'])->name('contactpage');
+Route::post('/contact', [MailController::class, 'contactSend'])->name('contactSend');
+
+
