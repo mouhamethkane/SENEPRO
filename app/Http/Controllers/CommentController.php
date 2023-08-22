97282a43535ccae4908136840bfbfc037d1676c7
unknown <gningmasseye10@gmail.com>
2023-08-22 09:25:39 +0000

    first commit

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+
+
+
+class CommentController extends Controller
+{
+    public function comments(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'prenom' => 'required',
+            'content' => 'required',
+        ]);
+        $comment = new Comment();
+        $comment->nom = $request->nom;
+        $comment->prenom = $request->prenom;
+        $comment->content = $request->content;
+
+        $comment->save();
+
+        return redirect('/profilcarrolleur')->with('status', 'votre commentaire à été bien enregistré.');
+    }
+
+}
