38ff5f5cd087bf3fbe736164c100fb86c16fa7e6
unknown <mouhamethkane25@gmail.com>
2023-08-25 14:37:07 +0200

    first co

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\MailController;
-use App\Http\Controllers\MetierController;
-use App\Http\Controllers\ouvrierController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,13 +16,9 @@ use App\Http\Controllers\ProfileController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-/*Route::get('/layouts.Crud.index', function () {
-    return view('layouts.Crud.index');
-});
-
-Route::get('/layouts.Crud.ajouter', function () {
-    return view('layouts.Crud.ajouter');
-});*/
+// Route::get('/', function () {
+//     return view('ouvriers.accueil');
+// });
 
 Route::get("/create",[ouvrierController::class,"index"])->name('ouvrier.create');
 Route::post("/ajouter",[ouvrierController::class,"ajouter"])->name('ouvrier.ajouter');
@@ -32,13 +28,13 @@ Route::get("/update/{id}",[ouvrierController::class,"update"])->name('crud.updat
 Route::post("/edit",[ouvrierController::class,"edit"])->name('crud.edit');
 Route::get("/crud.delete/{id}",[ouvrierController::class,"delete"])->name('crud.delete');
 
-Route::get('/', function () {
-    return view('ouvriers.accueil');
-});
-
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
+
+Route::get('/article', function () {
+    return view('article');
+})->name('index');
 
 Route::get('/accueil', function () {
     return view('ouvriers.accueil');
@@ -95,6 +91,95 @@ Route::get('/platrier', function () {
     return view('ouvriers.platrier');
 })->name('platrier');
 
+Route::get('/plombier', function () {
+    return view('ouvriers.plombier');
+})->name('plombier');
+
+Route::get('/tapissier', function () {
+    return view('ouvriers.tapissier');
+})->name('tapissier');
+
+Route::get('/profilculte', function () {
+    return view('ouvriers.profilculte');
+})->name('profilculte');
+
+Route::get('/coifprofil', function () {
+    return view('ouvriers.coifprofil');
+})->name('coifprofil');
+
+Route::get('/meuniprofil', function () {
+    return view('ouvriers.meuniprofil');
+})->name('meuniprofil');
+
+
+Route::get('/profiltraiteur', function () {
+    return view('ouvriers.profiltraiteur');
+})->name('profiltraiteur');
+
+Route::get('/tailleurprofil', function () {
+    return view('ouvriers.tailleurprofil');
+})->name('tailleurprofil');
+
+Route::get('/chef_cousinerprofil', function () {
+    return view('ouvriers.chef_cousinerprofil');
+})->name('chef_cousinerprofil');
+
+Route::get('/electricienprofil', function () {
+    return view('ouvriers.electricienprofil');
+})->name('electricien');
+
+Route::get('/maquiprofil', function () {
+    return view('ouvriers.maquiprofil');
+})->name('maquilleuse');
+
+Route::get('/tapiserprofil', function () {
+    return view('ouvriers.tapiserprofil');
+})->name('tapiserprofil');
+
+Route::get('/plombierprofil', function () {
+    return view('ouvriers.plombierprofil');
+})->name('plombierprofil');
+
+Route::get('/profilplatre', function () {
+    return view('ouvriers.profilplatre');
+})->name('profilplatre');
+
+Route::get('/mecaniprofil', function () {
+    return view('ouvriers.mecaniprofil');
+})->name('mecanicien');
+
+Route::get('/chauffeur', function () {
+    return view('ouvriers.profilchauffeur');
+})->name('chauffeur');
+
+Route::get('/traiteur', function () {
+    return view('ouvriers.traiteur');
+})->name('traiteur');
+
+Route::get('/tailleur', function () {
+    return view('ouvriers.tailleur');
+})->name('tailleur');
+
+Route::get('/cuisier', function () {
+    return view('ouvriers.cuisier');
+})->name('cuisier');
+
+Route::get('/peintre', function () {
+    return view('ouvriers.peintre');
+})->name('peintre');
+
+Route::get('/coiffureh', function () {
+    return view('ouvriers.coiffureh');
+})->name('coiffureh');
+
+Route::get('/menuisier', function () {
+    return view('ouvriers.menuisier');
+})->name('menuisier');
+
+Route::get('/culte', function () {
+    return view('ouvriers.culte');
+})->name('culte');
+
 Route::get('/dashboard', function(){
     return view('dashboard');
 })->name('dashboard');
@@ -103,13 +188,28 @@ Route::middleware('auth')->group(function(){
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profole.edit');
     Route::get('/profile', [ProfileController::class, 'update'])->name('profole.update');
     Route::get('/profile', [ProfileController::class, 'destroy'])->name('profole.destroy');
-
 });
 
 
 Route::get('/contact', [MailController::class, 'contact'])->name('contactpage');
 Route::post('/contact', [MailController::class, 'contactSend'])->name('contactSend');
 
+
+Route::get('/article', [ArticleController::class, 'index'])->name('index');
+Route::get('/consulte', [ArticleController::class, 'consulte'])->name('consulte');
+Route::post('/article', [ArticleController::class, 'store'])->name('article.store');
+Route::get('/edit/{id}', [ArticleController::class, 'edit'])->name('edit');
+Route::post('/update/{id}', [ArticleController::class, 'update'])->name('update');
+
+Route::get('/delete/{id}', [ArticleController::class, 'delete'])->name('delete');
+
+Route::get('/', [ArticleController::class, 'accueil'])->name('accueil');
+Route::get('/article/{id}', [ArticleController::class, 'show']);
+
+Route::post('/comments', [CommentController::class, 'comments']);
+Route::get('profilcarrolleur', [CommentController::class, 'profilcarrolleur']);
+
+// *******************************************************************
 Route::get('/delete-etudiant/{id}', [EtudiantController::class, 'delete_etudiant']);
 Route::get('/update-etudiant/{id}', [EtudiantController::class, 'update_etudiant']);
 Route::post('/update/traitement', [EtudiantController::class, 'update_etudiant_traitement']);
@@ -126,9 +226,12 @@ Route::controller(MetierController::class)->group(function () {
     Route::get('/metiers.edit/{id}/edit', 'edit')->name('metiers.edit');
     Route::post('/metiers.update/{id}', 'update')->name('metiers.update');
     Route::post('/metiers.store', 'store')->name('metiers.store');
-    Route::delete('/metiers.destroy/{id}', 'destroy')->name('metiers.destroy');;
+    Route::delete('/metiers.destroy/{id}', 'destroy')->name('metiers.destroy');
+    });
 
-});
+
+
+
 
 
 
